806f47257b8445797eb4c9ec1c101ec08358e951
Nickolas Burr <nickolasburr@gmail.com>
2019-01-25 16:51:45 -0500

    Add Http\Endpoint model

---
+++ Model/Http/Endpoint.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Endpoint.php
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the MIT License, which
+ * is bundled with this package in the file LICENSE.txt.
+ *
+ * It is also available on the Internet at the following URL:
+ * https://docs.auroraextensions.com/magento/extensions/2.x/magentocroncloudfunctions/LICENSE.txt
+ *
+ * @package       AuroraExtensions_MagentoCronCloudFunctions
+ * @copyright     Copyright (C) 2019 Aurora Extensions <support@auroraextensions.com>
+ * @license       MIT License
+ */
+
+namespace AuroraExtensions\MagentoCronCloudFunctions\Model\Http;
+
+class Endpoint
+{
+}
