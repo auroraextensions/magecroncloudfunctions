1fe2beed29d50c2c8610e1bf4ea5a12527e4ea66
Nickolas Burr <nickolasburr@gmail.com>
2019-02-02 17:15:29 -0500

    Add Http\TransportInterface, Http\Transport

---
+++ Model/Http/Transport.php
@@ -0,0 +1,68 @@
+<?php
+/**
+ * Transport.php
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the MIT License, which
+ * is bundled with this package in the file LICENSE.txt.
+ *
+ * It is also available on the Internet at the following URL:
+ * https://docs.auroraextensions.com/magento/extensions/2.x/magentocroncloudfunctions/LICENSE.txt
+ *
+ * @package       AuroraExtensions_MagentoCronCloudFunctions
+ * @copyright     Copyright (C) 2019 Aurora Extensions <support@auroraextensions.com>
+ * @license       MIT License
+ */
+
+namespace AuroraExtensions\MagentoCronCloudFunctions\Model\Http;
+
+use Zend\Http\Client as HttpClient;
+
+class Transport implements TransportInterface
+{
+    /** @property HttpClient $client */
+    protected $client;
+
+    /** @property array $defaultOptions */
+    public static $defaultOptions = [
+        'adapter' => HttpClient\Adapter\Curl::class,
+        'timeout' => 30,
+    ];
+
+    /**
+     * @param HttpClient $client
+     * @return void
+     */
+    public function __construct(HttpClient $client)
+    {
+        $this->client = $client;
+    }
+
+    /**
+     * Set HTTP client options.
+     *
+     * @param array $options
+     * @return $this
+     */
+    public function setOptions(array $options = [])
+    {
+        $this->client->setOptions($options);
+
+        return $this;
+    }
+
+    /**
+     * Send HTTP request via specified transport.
+     *
+     * @param RequestInterface $request
+     * @return $this
+     * @todo: Consider ResponseInterface, return response object to caller.
+     */
+    public function send(RequestInterface $request)
+    {
+        $this->client->send($request->getHttpRequest());
+
+        return $this;
+    }
+}
