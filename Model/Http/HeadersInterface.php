4aecb75659a67adc26527c19ddd9a1443613dd6f
Nickolas Burr <nickolasburr@gmail.com>
2019-02-02 15:43:37 -0500

    Add Http\HeadersInterface, Http\Headers

---
+++ Model/Http/HeadersInterface.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * HeadersInterface.php
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the MIT License, which
+ * is bundled with this package in the file LICENSE.txt.
+ *
+ * It is also available on the Internet at the following URL:
+ * https://docs.auroraextensions.com/magento/extensions/2.x/magentocroncloudfunctions/LICENSE.txt
+ *
+ * @package       AuroraExtensions_MagentoCronCloudFunctions
+ * @copyright     Copyright (C) 2019 Aurora Extensions <support@auroraextensions.com>
+ * @license       MIT License
+ */
+
+namespace AuroraExtensions\MagentoCronCloudFunctions\Model\Http;
+
+interface HeadersInterface
+{
+    /**
+     * @param array $headers
+     * @return $this
+     */
+    public function addHeaders(array $headers);
+}
